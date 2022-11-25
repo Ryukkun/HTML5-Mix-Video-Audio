92626ac51e81b6856ad33fd1dfb01732bd1cf088
Ryukkun <83561145+Ryukkun@users.noreply.github.com>
2022-11-25 17:51:55 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+(async () => {
+  const { createFFmpeg } = FFmpeg
+
 async function mergeVideo(video, audio) {
     let { createFFmpeg, fetchFile } = FFmpeg;
     let ffmpeg = createFFmpeg();
@@ -8,3 +11,46 @@ async function mergeVideo(video, audio) {
     let data = await ffmpeg.FS('readFile', 'output.mp4');
     return new Uint8Array(data.buffer);
 };
+
+  async function generateVideo(images) {
+    const ffmpeg = createFFmpeg({ log: true })
+    await ffmpeg.load()
+
+    images.forEach(async (image, i) => {
+      await ffmpeg.write(`image${i}.png`, image)
+    })
+
+    await ffmpeg.run('-r 1 -i image%d.png -pix_fmt yuv420p output.mp4')
+    const data = ffmpeg.read('output.mp4')
+    return data
+  }
+
+  function createObjectUrl(array, options) {
+    const blob = new Blob(array, options)
+    const objectUrl = URL.createObjectURL(blob)
+    return objectUrl
+  }
+
+  function insertVideo(src) {
+    const video = document.createElement('video')
+    video.controls = true
+
+    video.onloadedmetadata = () => {
+      document.body.appendChild(video)
+    }
+
+    video.src = src
+  }
+
+  const div = document.createElement('div')
+  div.innerText = '動画生成中'
+  document.body.appendChild(div)
+
+  const images = generateImages()
+  const video = await generateVideo(images)
+  const objectUrl = createObjectUrl([video], { type: 'video/mp4' })
+  insertVideo(objectUrl)
+
+  document.body.removeChild(div)
+})()
+
