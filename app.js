a03a64ef8fefc4abeb176fb7e71477a297561339
Ryukkun <83561145+Ryukkun@users.noreply.github.com>
2022-11-25 17:27:28 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+async function mergeVideo(video, audio) {
+    let { createFFmpeg, fetchFile } = FFmpeg;
+    let ffmpeg = createFFmpeg();
+    await ffmpeg.load();
+    ffmpeg.FS('writeFile', 'video.mp4', await fetchFile(video));
+    ffmpeg.FS('writeFile', 'audio.mp4', await fetchFile(audio));
+    await ffmpeg.run('-i', 'video.mp4', '-i', 'audio.mp4', '-c', 'copy', 'output.mp4');
+    let data = await ffmpeg.FS('readFile', 'output.mp4');
+    return new Uint8Array(data.buffer);
+};
